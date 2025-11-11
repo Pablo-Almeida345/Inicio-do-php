4d38bfaa714fc56687afeeadac8ca7dd3d54d479
Pablo-Alemida345 <pabloalmeidadasilva@eaportal.org>
2025-11-11 15:25:54 -0300

    Terceiro commit
    Operadores

--- index.php
+++ index.php
@@ -7,14 +7,42 @@ print 'Canal ti2'
 var_dump alem de exibir o texto ele exibe o valor da variavel e a quantidade de palavras 
 var_dump('Canal ti2');*/
 
-//Variaveis
+/*Variaveis
 $nome = 'Canal TI '; //string
 $idade = 25;//int
 $valor = 25.9;//float
 $booleana = false;//boolean
 
+Concatenação (Basicamente e usar vairas variaveis)
+echo $nome . 'idade:' . $idade;*/
 
-echo $nome . 'idade:' . $idade;
+/*Operadores*/
+$a = 10;
+$b = 5;
 
+// +, -, *, /, %
 
+/*echo $a + $b;
+echo $a - $b;
+echo $a * $b;
+echo $a / $b;
+echo $a % $b;*/
+
+// ==, !=, ===, !==, <, >, <=, >=
+var_dump($a == $b);
+var_dump($a != $b);
+var_dump($a === $b);
+var_dump($a !== $b);
+var_dump($a < $b);
+var_dump($a > $b);
+var_dump($a <= $b);
+var_dump($a >= $b);
+
+// && (E), || (OU) e ! (NÃO)
+$c = true;
+$d = true;
+
+var_dump($c && $d);
+var_dump($c || $d);
+var_dump(!$d);
 ?>
