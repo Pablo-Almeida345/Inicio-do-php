8133bab12c646fdcb4e2bce4cc82a8d434ce52f3
Pablo-Alemida345 <pabloalmeidadasilva@eaportal.org>
2025-11-11 15:31:09 -0300

    Quarto commit
    Funções

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//Primeiro commit 
 /*Mostrar textos na tela 
 echo e print auxilia ao mostrar texto na tela 
 echo 'canal ti';  
@@ -7,6 +7,7 @@ print 'Canal ti2'
 var_dump alem de exibir o texto ele exibe o valor da variavel e a quantidade de palavras 
 var_dump('Canal ti2');*/
 
+//Segundo commit 
 /*Variaveis
 $nome = 'Canal TI '; //string
 $idade = 25;//int
@@ -16,7 +17,8 @@ $booleana = false;//boolean
 Concatenação (Basicamente e usar vairas variaveis)
 echo $nome . 'idade:' . $idade;*/
 
-/*Operadores*/
+//Terceiro commit 
+/*Operadores
 $a = 10;
 $b = 5;
 
@@ -26,7 +28,7 @@ $b = 5;
 echo $a - $b;
 echo $a * $b;
 echo $a / $b;
-echo $a % $b;*/
+echo $a % $b;
 
 // ==, !=, ===, !==, <, >, <=, >=
 var_dump($a == $b);
@@ -44,5 +46,13 @@ $d = true;
 
 var_dump($c && $d);
 var_dump($c || $d);
-var_dump(!$d);
+var_dump(!$d);*/
+
+//Quarto commit 
+/*Funções*/
+function saudacao($nome, $idade){
+    return "Olá, $nome, você tem $idade anos";
+}
+
+echo saudacao("Canal TI", 20);
 ?>
