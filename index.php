e420991d12db9af1321c08a0edb0bc5ee824005f
Pablo-Alemida345 <pabloalmeidadasilva@eaportal.org>
2025-11-11 15:06:31 -0300

    Segundo commit
    Agora tenho a experiencia de aprender a usar variaveis

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
 
-/* echo e print auxilia ao mostrar texto na tela 
+/*Mostrar textos na tela 
+echo e print auxilia ao mostrar texto na tela 
 echo 'canal ti';  
-print 'Canal ti2' */
+print 'Canal ti2'
+var_dump alem de exibir o texto ele exibe o valor da variavel e a quantidade de palavras 
+var_dump('Canal ti2');*/
 
-//var_dump alem de exibir o texto ele exibe o valor da variavel e a quantidade de palavras 
-//var_dump('Canal ti2');
-namespace App\Http\Livewire;
- 
-use Livewire\Component;
- 
-class Counter extends Component
-{
-    public $count = 0;
- 
-    public function increment()
-    {
-        $this->count++;
-    }
- 
-    public function render()
-    {
-        return view('livewire.counter');
-    }
-}
+//Variaveis
+$nome = 'Canal TI '; //string
+$idade = 25;//int
+$valor = 25.9;//float
+$booleana = false;//boolean
+
+
+echo $nome . 'idade:' . $idade;
+
 
 ?>
